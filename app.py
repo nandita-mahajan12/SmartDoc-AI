6d7890476a7de02d504c4af1495c3612a1e02e06
nandita-mahajan12 <nanditamahajan32@gmail.com>
2026-02-11 17:34:36 +0530

    SmartDoc AI - Final Stable Version

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import numpy as np
-import faiss
 from pypdf import PdfReader
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
-st.set_page_config(page_title="SmartDoc AI - Similarity Search & RAG System")
+# Page Config
+st.set_page_config(page_title="SmartDoc AI - RAG System")
 st.title("SmartDoc AI - Similarity Search & RAG System")
 
-# Load embedding model (lightweight + stable)
+# Load Embedding Model (Lightweight + Stable)
 @st.cache_resource
 def load_model():
     return SentenceTransformer("all-MiniLM-L6-v2")
@@ -24,7 +25,10 @@ if uploaded_file is not None:
     text = ""
 
     for page in pdf_reader.pages:
-        text += page.extract_text()
+        page_text = page.extract_text()
+        if page_text:
+                text += page_text.replace("\n", " ")
+
 
     st.success("PDF Loaded Successfully!")
 
@@ -37,34 +41,25 @@ if uploaded_file is not None:
 
     chunks = chunk_text(text)
 
-    # Generate embeddings
-    embeddings = model.encode(chunks)
-
-    # Convert to numpy float32
-    embeddings = np.array(embeddings).astype("float32")
-
-    # Create FAISS index
-    dimension = embeddings.shape[1]
-    index = faiss.IndexFlatL2(dimension)
-    index.add(embeddings)
+    # Generate embeddings for document chunks
+    doc_embeddings = model.encode(chunks)
 
     st.success("Document Indexed!")
 
-    # Ask Question
-    question = st.text_input("Ask a question about this document")
+    # User Question
+    query = st.text_input("Ask a question about this document")
 
-    if question:
-        query_embedding = model.encode([question])
-        query_embedding = np.array(query_embedding).astype("float32")
+    if query:
 
-        D, I = index.search(query_embedding, k=3)
+        # Create embedding for query
+        query_embedding = model.encode([query])
 
-        st.subheader("Top Relevant Chunks:")
-        context = ""
+        # Calculate similarity
+        scores = cosine_similarity(query_embedding, doc_embeddings)[0]
 
-        for idx in I[0]:
-            st.write(chunks[idx])
-            context += chunks[idx] + "\n\n"
+        # Get most relevant chunk
+        best_index = np.argmax(scores)
+        best_chunk = chunks[best_index]
 
-        st.subheader("Answer:")
-        st.success(context)
+        st.subheader("Most Relevant Context:")
+        st.write(best_chunk)
